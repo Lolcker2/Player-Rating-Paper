3402b8760a51c1c0def6d0ff12c232eb5ba6b06c
Lolcker2 <eyal.locker2006@gmail.com>
2026-05-03 22:58:23 +0300

    started predictive test

--- Simulate.py
+++ Simulate.py
@@ -1,29 +1,42 @@
-
+import Models.CoVModel as cov
+import Models.SigmaModel as smodel
 
 filename = 'Magnus Carlsen-100_matches'
 data = open(r'Cache/'+filename+'.txt', "r").read().split('\n')
-print(data)
+# print(data)
 
-ResultWeights = {"1-0": 1, "½-½": 0, "0-1": -1}
+ResultWeights = {"1-0": 1, "½-½": 0, "0-1": -1, "Â½-Â½": 0}
 
-def reOrient(match: list, name: str):
+def reOrient(_match: list, _name: str):
     if _match[1][0] == _name:
         return [_match[1], _match[0], -1 * _match[2]]
     return  _match
 
-def getMatch(_data: list, _index: int)->list:
-    match, result = _data[_index].split('|')
+def getMatch(_data: list, _index: int, _match: list = False)->list:
+    match, result = _match.split('|') if _match else _data[_index].split('|')
     p1, p2 = [i.split(',') for i in match.split("VS")]
     p1[1], p2[1] = int(p1[1]), int(p2[1])
     p2[0] = p2[0].lstrip()
     return [p1, p2, ResultWeights[result.strip()]]
 
-print(getMatch(data, 0))
+#print(getMatch(data, 0))
 
 
 
+def predictiveTest(data):
+    for match in data:
+        result = reOrient(getMatch([], 0, match), "Magnus Carlsen")
+        print(result)
+
+
+predictiveTest(data)
 
 """
+divide into 2 parts:
+    predictive elo vs stoch on historical data
+    convergance - epoch + evolution
+
+
 todo: find where i use the model and outsource it to a standalone file
 note look at Model and Model2 see if there's anything useful.
 
